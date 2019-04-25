e6dc474cabfc9dd89c7ea19c820b7dcabd982a15
onildo <onildo@norteldata.com.br>
2019-04-25 15:30:30 -0300

    Chap 6 Linked Lists

--- 6-linkedlists.js
+++ 6-linkedlists.js
@@ -5,8 +5,10 @@
  * ponteiro para o próximo elemento.
  *
  * Inserir ou remover elementos no início ou no meio de um array é caro porque
- * precisa-se shiftar todos os outros elementos. Na lista ligada é barato, pois
- * basta atualizar um ponteiro.
+ * precisa-se shiftar todos os outros elementos. Na lista ligada é mais barato,
+ * pois basta atualizar um ponteiro (mas não se engane. Não é O(1) porque
+ * continua sendo necessário iterar desde a cabeça da lista para achar o
+ * elemento a ser removido.
  *
  * Por outro lado, para acessar um elemento do array basta entrar com seu
  * índice; enquanto para a lista ligada é preciso iterar por todos os elementos,
@@ -37,7 +39,93 @@ class Node {
  * palavra 'value', que é mais especifica.
  */
 
-//Vamos criar o método push, que adiciona um elemento ao fim da lista
+//Vamos criar o método push, que adiciona um elemento ao fim da lista ligada
+class LinkedList {
+	constructor() {
+		this.head = undefined
+		this.count = 0
+	}
+	
+	push(element) {
+		if (this.count > 0) {
+			let current = this.head
+			
+			for(let i = 1; i < this.count; i++) {
+				current = current.next
+			}
+			
+			current.next = new Node(element)
+		} else {
+			this.head = new Node(element)
+		}
+		
+		this.count += 1
+	}
+}
+
+class Node {
+	constructor(element) {
+		this.element = element
+		this.next = undefined
+	}
+}
+
+//Vamos criar um método para remover um elemento de um índice da lista ligada
+class LinkedList {
+	constructor() {
+		this.head = undefined
+		this.count = 0
+	}
+	
+	push(element) {
+		if (this.count > 0) {
+			let current = this.head
+			
+			for(let i = 1; i < this.count; i++) {
+				current = current.next
+			}
+			
+			current.next = new Node(element)
+		} else {
+			this.head = new Node(element)
+		}
+		
+		this.count += 1
+	}
+	
+	removeAt(index) {
+		if (index >= 0 && index < this.count) {
+			if (index === 0) {
+				this.head = this.head.next
+			} else {
+				let previous = undefined
+				let current = this.head
+				
+				for(let i = 0; i < index; i++) {
+					previous = current
+					current = current.next
+				}
+				
+				previous.next = current.next
+			}
+			
+			this.count--
+		}
+	}
+}
+
+class Node {
+	constructor(element) {
+		this.element = element
+		this.next = undefined
+	}
+}
+/* Em removeAt(index) o objeto em current será removido pelo garbage collector
+ */
+
+//Como todas as funções de lista ligada precisarão iterar desde a cabeça até o
+//elemento que queremos, podemos isolar essa parte da função removeAt(index) e
+//criar uma função auxiliar getElementAt(index)
 class LinkedList {
 	constructor() {
 		this.head = undefined
@@ -56,7 +144,218 @@ class LinkedList {
 		} else {
 			this.head = new Node(element)
 		}
+		this.count++
+	}
+	
+	removeAt(index) {
+		if (index >= 0 && index < this.count) {
+			if (index === 0) {
+				this.head = this.head.next
+			} else {
+				let previous = this.getElementAt(index - 1)
+				let current = previous.next
+				previous.next = current.next
+			}
+			
+			this.count--
+		}
+	}
+	
+	getElementAt(index) {
+		let node = undefined
+
+		if (index >= 0 && index < this.count) {
+			node = this.head
+			
+			for (let i = 1; i < index + 1; i++) {
+				node = node.next
+			}
+		}
+		
+		return node
+	}
+}
+
+class Node {
+	constructor(element) {
+		this.element = element
+		this.next = undefined
+	}
+}
+
+//Agora uma função insert que põe num índice específico
+class LinkedList {
+	constructor() {
+		this.head = undefined
+		this.count = 0
+	}
+	
+	push(element) {
+		if (this.count > 0) {
+			let last = this.getElementAt(this.count - 1)
+			last.next = new Node(element)
+		} else {
+			this.head = new Node(element)
+		}
+		
 		this.count += 1
+	}
+	
+	removeElementAt(index) {
+		if (index >= 0 && index < this.count) {
+			if (index === 0) {
+				this.head = this.head.next
+			} else {
+				let previous = this.getElementAt(index - 1)
+				let current = previous.next
+				previous.next = current.next
+			}
+			this.count--
+		}
+	}
+	
+	insert(element, index) {
+		if (index >= 0 && index <= this.count) {
+			let node = new Node(element)
+			
+			if (index === 0) {
+				node.next = this.head
+				this.head = node
+			} else {
+				let previous = this.getElementAt(index - 1)
+				let current = previous.next
+				node.next = current
+				previous.next = node
+			}
+			
+			this.count++
+			return true
+		}
+		
+		return false
+	}
+	
+	getElementAt(index) {
+		let node = undefined
+		
+		if (index >= 0 && index < this.count) {
+			node = this.head
+			
+			for (let i = 1; i < index + 1; i++) {
+				node = node.next
+			}
+		}
+		
+		return node
+	}
+}
+
+class Node {
+	constructor(element) {
+		this.element = element
+		this.next = undefined
+	}
+}
+/* Em insert retornamos um booleano para indicar se foi out of bounds */
+
+// Função indexOf que retorna -1 se não for encontrado nó com o elemento
+// e função toString
+class LinkedList {
+	constructor() {
+		this.head = undefined
+		this.count = 0
+	}
+	
+	push(element) {
+		if (this.count > 0) {
+			let last = this.getElementAt(this.count - 1)
+			last.next = new Node(element)
+		} else {
+			this.head = new Node(element)
+		}
+			
+		this.count += 1
+	}
+	
+	removeElementAt(index) {
+		if (index >= 0 && index < this.count) {
+			if (index === 0) {
+				this.head = this.head.next
+			} else {
+				let previous = this.getElementAt(index - 1)
+				let current = previous.next
+				previous.next = current.next
+			}
+			
+			this.count--
+		}
+	}
+	
+	insert(element, index) {
+		if (index >= 0 && index <= this.count) {
+			let node = new Node(element)
+			
+			if (index === 0) {
+				node.next = this.head
+				this.head = node
+			} else {
+				let previous = this.getElementAt(index - 1)
+				let current = previous.next
+				node.next = current
+				previous.next = node
+			}
+			
+			this.count++
+			return true
+		}
+		
+		return false
+	}
+	
+	getElementAt(index) {
+		let node = undefined
+		
+		if (index >= 0 && index < this.count) {
+			node = this.head
+			
+			for (let i = 1; i < index + 1; i++) {
+				node = node.next
+			}
+		}
+		
+		return node
+	}
+	
+	indexOf(element) {
+		let current = this.head
+		
+		for(let i = 0; i < this.count; i++) {
+			if (current.element === element) {
+				return i
+			}
+			
+			current = current.next
+		}
+		
+		return -1
+	}
+	
+	toString() {
+		let result = ''
+		
+		if (this.count === 0) {
+			return result
+		} else {
+			let node = this.head
+			result = `${node.element}`
+			
+			for(let i = 1; i < this.count; i++) {
+				node = node.next
+				result += `,${node.element}`
+			}
+			
+			return result
+		}
 	}
 }
 
