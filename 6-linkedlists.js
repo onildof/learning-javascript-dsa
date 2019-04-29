b103941964502368b8f9a5a3b96cace0e7e41e8f
onildo <onildo@norteldata.com.br>
2019-04-29 14:18:22 -0300

    Chapter 6 finished

--- 6-linkedlists.js
+++ 6-linkedlists.js
@@ -8,11 +8,14 @@
  * precisa-se shiftar todos os outros elementos. Na lista ligada é mais barato,
  * pois basta atualizar um ponteiro (mas não se engane. Não é O(1) porque
  * continua sendo necessário iterar desde a cabeça da lista para achar o
- * elemento a ser removido.
+ * elemento a ser removido).
  *
  * Por outro lado, para acessar um elemento do array basta entrar com seu
  * índice; enquanto para a lista ligada é preciso iterar por todos os elementos,
  * usando os ponteiros desde a cabeça (head).
+ *
+ * Em suma, quando se quer adicionar e remover muitos elementos, a lista é
+ * mais barata.
  */
 
 //Vamos criar uma classe LinkedList
@@ -368,7 +371,6 @@ class Node {
 
 //DOUBLY LINKED LIST. Cada nó tem um ponteiro pro nó anterior
 //e a classe tem um ponteiro para o último nó também
-
 class DoublyLinkedList extends LinkedList {
 	constructor() {
 		super()
@@ -442,113 +444,6 @@ class DoublyNode extends Node {
 }
 
 // Circular Linked Lists
-
-class LinkedList {
-	constructor() {
-		this.head = undefined
-		this.count = 0
-	}
-	
-	push(element) {
-		if (this.count > 0) {
-			let last = this.getElementAt(this.count - 1)
-			last.next = new Node(element)
-		} else {
-			this.head = new Node(element)
-		}
-			
-		this.count += 1
-	}
-	
-	removeElementAt(index) {
-		if (index >= 0 && index < this.count) {
-			if (index === 0) {
-				this.head = this.head.next
-			} else {
-				let previous = this.getElementAt(index - 1)
-				let current = previous.next
-				previous.next = current.next
-			}
-			
-			this.count--
-		}
-	}
-	
-	insert(element, index) {
-		if (index >= 0 && index <= this.count) {
-			let node = new Node(element)
-			
-			if (index === 0) {
-				node.next = this.head
-				this.head = node
-			} else {
-				let previous = this.getElementAt(index - 1)
-				let current = previous.next
-				node.next = current
-				previous.next = node
-			}
-			
-			this.count++
-			return true
-		}
-		
-		return false
-	}
-	
-	getElementAt(index) {
-		let node = undefined
-		
-		if (index >= 0 && index < this.count) {
-			node = this.head
-			
-			for (let i = 1; i < index + 1; i++) {
-				node = node.next
-			}
-		}
-		
-		return node
-	}
-	
-	indexOf(element) {
-		let current = this.head
-		
-		for(let i = 0; i < this.count; i++) {
-			if (current.element === element) {
-				return i
-			}
-			
-			current = current.next
-		}
-		
-		return -1
-	}
-	
-	toString() {
-		let result = ''
-		
-		if (this.count === 0) {
-			return result
-		} else {
-			let node = this.head
-			result = `${node.element}`
-			
-			for(let i = 1; i < this.count; i++) {
-				node = node.next
-				result += `,${node.element}`
-			}
-			
-			return result
-		}
-	}
-}
-
-class Node {
-	constructor(element) {
-		this.element = element
-		this.next = undefined
-	}
-}
-
 class CircularLinkedList extends LinkedList {
 	constructor() {
 		super()
@@ -609,5 +504,105 @@ class CircularLinkedList extends LinkedList {
 		}
 		
 		return false
+	}
+}
+
+//Sorted Linked Lists
+//Ao invés de usar um algoritmo de ordenação, inserimos o elemento sempre na
+//posição correta.
+function defaultCompare(a, b) {
+	if (a === b) {
+		return 0
+	}
+	
+	return a < b ? -1 : 1
+}
+
+class SortedLinkedList extends LinkedList {
+	constructor(compareFn = defaultCompare) {
+		super()
+		this.compareFn = compareFn
+	}
+}
+/*Se você não entregar uma função de comparação ao construtor, ele usará
+ *a defaultCompare. A idéia é permitir que para objetos mais complexos,
+ *o programador use uma função de comparação adequada na hora de construir a
+ *lista ligada ordenada.
+ */
+
+//Para deixar o código do comparador mais legível (?) usaremos uma constante
+//Compare para representar em texto os valores.
+//O único método que modificaremos da classe LinkedList é o de inserir
+const Compare = {
+	LESS_THAN: -1,
+	BIGGER_THAN: 1
+}
+
+function defaultCompare(a, b) {
+	if (a === b) {
+		return 0
+	}
+	
+	return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+class SortedLinkedList extends LinkedList {
+	constructor(compareFn = defaultCompare) {
+		super()
+		this.compareFn = compareFn
+	}
+	
+	insert(element, index = 0) {
+		if (this.count === 0) {
+			return super.insert(element, 0)
+		}
+		
+		const pos = this.getIndexNextSortedElement(element)
+		return super.insert(element, pos)
+	}
+	
+	getIndexNextSortedElement(element) {
+		let current = this.head
+		let i = 0
+		
+		for(; i < this.count; i++) {
+			const comp = this.compareFn(element, current.element)
+			
+			if (comp === Compare.LESS_THAN) {
+				return i
+			}
+		}
+		
+		return i
+	}
+}
+/* Como é uma lista ordenada, o usuário não tem dizer sobre o index da inserção,
+ * por isso fizemos index = 0 na assinatura da função insert.
+ */
+
+//É posível usar a lista ligada para implementar uma pilha, fila, ou deque
+class StackLinkedList {
+	constructor() {
+		this.items = new DoublyLinkedList()
+	}
+	
+	push(element) {
+		this.items.insert(element, this.size())
+	}
+	
+	pop() {
+		if (this.isEmpty()) {
+			return undefined
+		}
+		
+		return this.items.removeElementAt(this.size() - 1)
+	}
+	
+	isEmpty() {
+		return this.items.isEmpty();
+	}
+	
+	size() {
+		return this.items.size();
 	}
 }
