3602088bb81798c0117e6b26174f4f4598f8f9ec
onildo <onildo@norteldata.com.br>
2019-05-15 15:11:55 -0300

    Chapter 8 Dictionaries and Hashes

--- 8-dictionaries-and-hashes.js
+++ 8-dictionaries-and-hashes.js
@@ -214,6 +214,7 @@ import { defaultToString } from './LearningJavaScriptDataStructuresandAlgorithms
 	}
 }*/
 
+/*
 console.log("test start")
 let d = new Dictionary()
 let nullshit = null
@@ -242,4 +243,361 @@ d.set("e", "enemy")
 d.set("f", "foxtrot")
 console.log(d.toString()) //[#d: delta], [#e: enemy], [#e: enemy]
 console.log("test end")
+*/
 
+/*
+ * Agora aprenderemos sobre a classe HashTable, também conhecida como HashMap,
+ * uma implementação hash da classe Dictionary
+ *
+ * Hashing é sobre retirar um valor de uma estrutura de dados no menor tempo
+ * possível. Quando usamos uma função hash, não precisamos iterar outros índices
+ * da estrutura de dados.
+ *
+ * Você aplica a função hash sobre a chave e obtém um valor que é usado como
+ * índice na estrutura de dados. Ou seja, a partir da chave você tem como
+ * automaticamente saber onde armazenar ou puxar o valor.
+ *
+ * A função hash aplicada a duas entradas diferentes pode resultar na mesma saída? Sim.
+ * É por isso que os locais da estrutura de dados são chamados de baldes (buckets)
+ * Então nessas horas de colisão se armazena mais de um valor no mesmo bucket.
+ * Aí iteraríamos dentro do mesmo índice, de fato; para evitar isso, temos de,
+ * baseados no domínio, escolher uma função hash boa que gere poucas colisões.
+ */
+
+class ValuePair {
+	constructor(key, value) {
+		this.key = key
+		this.value = value
+	}
+	
+	toString() {
+		return `[#${this.key}: ${this.value}]`
+	}
+}
+
+class HashTable {
+	constructor(toStrFn = defaultToString) {
+		this.toStrFn =  toStrFn
+		this.table = {}
+	}
+	
+	loseloseHashCode(key) {
+		if (typeof key === 'number') {
+			return key
+		}
+		
+		const tableKey = this.toStrFn(key)
+		let hash = 0
+		for (let i = 0; i < tableKey.length; i++) {
+			hash += tableKey.charCodeAt(i)
+		}
+		
+		return hash % 37 //Esse módulo só serve para evitar números grandes que não caibam numa variável numérica
+	}
+	
+	hashCode(key) {
+		return this.loseloseHashCode(key)
+	}
+	
+	/* A lógica de put é similar à de set da classe Dictionary. Mas a convenção
+	 * para Hashtable é chamar essa função de put. Então tá.
+	 */
+	put(key, value) {
+		if (key != null && value != null) {
+			const position = this.hashCode(key)
+			this.table[position] = new ValuePair(key, value) //também vamos armazenar a chave para propósitos informacionais
+			return true
+		}
+		
+		return false
+	}
+	
+	get(key) {
+		const position = this.hashCode(key)
+		const valuePair = this.table[position]
+		
+		return valuePair == null ? undefined : valuePair.value
+	}
+	
+	remove(key) {
+		const position = this.hashCode(key)
+		const valuePair = this.table[position]
+		
+		if (valuePair != null) {
+			delete this.table[position]
+			return true
+		}
+		
+		return false
+	}
+}
+
+/* Então a diferença de HashTable para Dictionary é que ao invés de armazenarmos
+ * o ValuePair na stringificação da key, passamos uma função hash na key para
+ * obter o índice onde o armazenaremos.
+ */
+
+//Testes:
+const hash = new HashTable()
+hash.put("Marrocos", "Rabate")
+hash.put("Argélia", "Argel")
+hash.put("Tunísia", "Tunes")
+console.log(hash.hashCode("Marrocos") + " - Marrocos")
+console.log(hash.hashCode("Argélia") + " - Argélia")
+console.log(hash.hashCode("Tunísia") + " - Tunísia")
+console.log(hash.get("Marrocos"))
+console.log(hash.get("Líbia"))
+console.log(hash.remove("Marrocos"))
+console.log(hash.get("Marrocos"))
+
+/* Hash Table vs Hash Set
+ * Em algumas linguagens de programação temos a estrutura Hash Set. É um Set que
+ * para inserir, remover ou pegar elementos, usamos uma função hashCode. E ao
+ * invés de inserirmos um par (key, value), inserimos só o value.
+ */
+
+/* Separate chaining
+ * Cria uma lista ligada para cada posição da tabela. É a técnica mais simples
+ * de lidar com colisões. A desvantagem é precisar de memória adicional fora da
+ * instância HashTable
+ */
+
+class HashTableSeparateChaining {
+	constructor(toStrFn = defaultToString) {
+		this.toStrFn = toStrFn
+		this.table = {}
+	}
+	
+	put(key, value) {
+		if (key != null && value != null) {
+			const position = this.hashCode(key)
+			
+			if (this.table[position] == null) {
+				this.table[position] = new LinkedList()
+			}
+			
+			this.table[position].push(new ValuePair(key, value))
+			return true
+		}
+		
+		return false
+	}
+	
+	get(key) {
+		const position = this.hashCode(key)
+		const linkedList = this.table[position]
+		
+		if (linkedlist != null && !linkedList.isEmpty()) {
+			let current = linkedList.getHead()
+			
+			while(current != null) {
+				if (current.element.key === key) {
+					return current.element.value
+				}
+				
+				current = current.next
+			}
+		}
+		
+		return undefined
+	}
+	
+	remove(key) {
+		const position = this.hashCode(key)
+		const linkedList = this.table[position]
+		
+		if (linkedList != null && !linkedList.isEmpty()) {
+			let current = linkedList.getHead()
+			
+			while(current != null) {
+				if (current.element.key === key) {
+					linkedList.remove(current.element)
+					
+					if (linkedList.isEmpty()) {
+						delete this.table[position]
+					}
+					
+					return true
+				}
+				
+				current = current.next
+			}
+		}
+		
+		return false
+	}
+}
+
+/* Linear Probing
+ * É outra forma de resolver as colisões da HashTable. Se chama linear porque
+ * os valores são armazenados diretamente na tabela, e não numa estrutura
+ * de dados separada.
+ *
+ * Se o índice estiver ocupado, armazenamos o par (key, value) no próximo
+ * índice da tabela.
+ *
+ * Por isso não podemos simplesmente remover um elemento de uma posição na
+ * tabela. Podemos usar uma flag no elemento (lazy or soft deletion), o que
+ * gradualmente vai deteriorar o desempenho da hash table.
+ *
+ * A outra forma é remover o elemento e shiftar os outros.
+ */
+
+class HashTableLinearProbing {
+	constructor(toStrFn = defaultToString) {
+		this.toStrFn = toStrFn
+		this.table = {}
+	}
+	
+	put(key, value) {
+		if (key != null && value != null) {
+			const position = this.hashCode(key)
+			
+			if (this.table[position] == null) {
+				this.table[position] = new ValuePair(key, value)
+			} else {
+				let index = position + 1
+				while (this.table[index] != null) {
+					index++
+				}
+				this.table[index] = new ValuePair(key, value)
+			}
+			
+			return true
+		}
+		
+		return false
+	}
+	
+	get(key) {
+		const position = this.hashCode(key)
+		
+		if (this.table[position] != null) {
+			if (this.table[position].key === key) {
+				return this.table[position].value
+			}
+			
+			let index = position + 1
+			
+			while (this.table[index] != null && this.table[index].key != key) {
+				index++
+			}
+			
+			if (this.table[index] != null && this.table[index].key === key) {
+				return this.table[index].value
+			}
+		}
+		
+		return undefined
+	}
+	
+	remove(key) {
+		const position = this.hashCode(key)
+		
+		if (this.table[position] != null) {
+			if (this.table[position].key === key) {
+				delete this.table[position]
+				this.verifyRemoveSideEffect(key, position)
+				
+				return true
+			}
+			
+			let index = position + 1
+			
+			while (this.table[index] != null && this.table[index].key != key) {
+				index++
+			}
+			
+			if (this.table[index] != null && this.table[index].key === key) {
+				delete this.table[index]
+				this.verifyRemoveSideEffect(key, position)
+				
+				return true
+			}
+		}
+		
+		return false
+	}
+	
+	verifyRemoveSideEffect(key, removedPosition) {
+		const hash = this.hashCode(key)
+		let index = removedPosition + 1
+		
+		while (this.table[index] != null) {
+			const posHash = this.hashCode(this.table[index].key)
+			
+			if (posHash <= hash || posHash <= removedPosition) {
+				this.table[removedPosition] = this.table[index]
+				delete this.table[index]
+				removedPosition = index
+			}
+			
+			index++
+		}
+	}
+}
+
+/* Todas essas soluções prejudicam o desempenho da hash table. O ideal é
+ * encontrar uma solução hash que evite colisões ao máximo.
+ *
+ * Uma boa função hash é a djb2:
+ */
+
+djb2HashCode(key) {
+	const tableKey = this.toStrFn(key) //transformamos a chave em uma string
+	let hash = 5831 //um número primo famoso
+	
+	for (let i = 0; i < tableKey.length; i++) {
+		hash = (hash * 33) + tableKey.charCodeAt(i) //mudamos o número
+	}
+	
+	return hash % 1013 //o número final é o módulo do menor primo maior que a tabela que queremos
+}
+
+/* ES2015 MAP CLASS
+ *
+ * A diferença entre esta e a nossa classe Dictionary é que os métodos values()
+ * e keys() retornam um Iterator ao invés de um array. Outra é que nossa classe
+ * Dictionary tinha um método size(), enquanto a classe nativa Map tem uma
+ * propriedade size.
+ */
+
+const map = new Map()
+map.set("Marrocos", "Rabate")
+map.set("Argélia", "Argel")
+map.set("Tunísia", "Tunis")
+map.set("Líbia", "Tripoli")
+console.log(map.has("Marrocos"))
+console.log(map.size)
+console.log(map.keys())
+console.log(map.values())
+console.log(map.get("Marrocos"))
+console.log(map.delete("Marrocos"))
+console.log(map.has(ob1))
+console.log(map.get(ob1))
+map.delete(ob1)
+
+/* ES2015 WEAKMAP AND WEAKSET CLASSES
+ *
+ * Além das novas estruturas Map e Set, o ECMAScript2015 trouxe as versões
+ * fracas dessas classes: WeakSet e WeakMap.
+ *
+ * As diferenças são que não existem métodos keys, values e entries, e que só
+ * é possível usar objetos como chaves.
+ *
+ * A razão de ser dessas duas classes é desempenho.
+ *
+ * Como não há método keys, values, ou entries. Se você não souber a chave,
+ * não tem como obter o valor.
+ */
+
+const map = new WeakMap()
+const ob1 = { country: "Marrocos"}
+const ob2 = { country: "Argélia"}
+const ob3 = { country: "Tunísia"}
+map.set(ob1, "Rabate")
+map.set(ob2, "Argel")
+map.set(ob3, "Tunis")
+console.log(map.has(ob1))
+console.log(map.get(ob1))
+map.delete(ob1)
